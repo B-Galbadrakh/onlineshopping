cd4b0911d522020fc4494083683a9321b50a0309
galbadrakh_b <b.galbadrah12@gmail.com>
2020-09-30 21:57:50 -0500

    initial commit ;

---
+++ src/main/java/com/shop/DemoOnlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOnlineshopApplication.class, args);
+	}
+
+}
